afd5a7f29558c59dd61cf28422bcf50b6329d2a2
atriak <vincent.lundell@elev.ga.ntig.se>
2023-08-22 09:46:08 +0200

    första inlämning

---
+++ module1.py
@@ -0,0 +1,51 @@
+'''
+
+
+print("hello world")
+
+print("hello world 'tuesday'")
+
+print("""
+      Lorem
+      Ipsum
+       Lorem 
+      Ipsum
+       Lorem
+      Ipsum
+       Lorem
+      Ipsum
+       Lorem
+      Ipsum
+        Lorem
+      Ipsum
+         Lorem
+         
+      """)
+
+
+print("resultat: \n 100%")
+
+print("hello world " + ",how are you?")
+
+print("sup bitches", ",lots of love")
+
+my_var = "medlemmar"
+my_var = my_var + ": johan"
+my_Var = my_var + ": vincent"
+
+print(my_var)
+
+name = input("namn: ")
+age = input("age: ")
+gender = input("gender: ")
+
+print(name +"\n"+ age +"\n"+ gender)
+
+
+
+name = input("hello, what is your name?: ")
+
+print("hello " + name + " welcome to this program!")
+'''
+
+
---
+++ pythoning.py
@@ -0,0 +1,32 @@
+'''print(10 // 3)
+print(10 / 3)    
+print(10 * 3)
+print(10 ** 3)
+print(100 ** 0.5)
+
+x = input("namn: ")
+age= input("år: ")
+
+
+print(x)
+print(age)
+
+print("hej", x, "du är", age, "år")'''
+
+'''print("""
+hej klassen 
+detta är fler radig print""")
+x = "johan"
+print(f'jag heter {x}')'''
+
+'''
+print("resultat: \n obama")
+'''
+
+
+
+float: int(first, secound)
+
+first
+
+print(first + secound)
